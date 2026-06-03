acd523bd98156cb2a0b27d2dd8c710bebf839628
MrMojangnt <jocksandmateovalladares@gmail.com>
2026-06-03 15:33:55 -0600

    mitad empresas

---
+++ Proyecto Juego/Empresas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Proyecto_Juego;
+
+//creando las empresas?
+public struct EmpresasNombres
+{
+    static string[] TecnologiaHardware = { "PineApple", "Sansung", "Votorola", "WhiteBerry", "Wuawei", "Siaomi",
+        "TELL", "Ovistar", "Celtel", "Oscuro", "HP", "Nvidia", "SteelSeries", "Whoow", "IBM", "Western Digital", 
+        "Vizio", "Red 7", "BUE Systems, Inc.", "Anazon", "Vast", "Sisco", "Samsara", "Apptronik", "HTC Global Services", "Micron Technology",
+        "LGC Electronics", "Shark", "Mony", "Goal", "Mason", "Biowater", "Canon", "Roku", "AMP", "Gogo", "True Anomaly", "Ambiq", "Turion Space",
+        "Caliola Engineering", "Machina Labs, Inc", "Graphcore", "Astus", "Altern Space", "Runwide", "ElectronQ", "Doodle Labs", "Protonauta", 
+        "Dandy", "Sierra Space", "Cenovo", "Halter", "Motive", "Logitech", "Sevasa", "Carbon Robotics", "Piaggio Fast Forward", 
+        "Inspiren", "AOAS"};
+    static string[] TecnologiaSoftware = { "Popstar Games","Gugul", "Nantendo", "CloseAI", "AntiMojang", "BoredGames", "LogicMax", "Equipo Cereza", 
+        "IEI Sports", "Ventanilla", "Macrosoft", "OCloud", "Spoorifly", "Disquettecord", "Walve", "Quantum Logic", "Unreel Ungine", "GitLub", "Dripbox",
+        "Virtual Forge", "AlphaWare", "CyberLeaf", "Code Republic", "Infinity Labs", "Nova Network", "Hyperlink Solutions", "Core Dynamics", 
+        "BlueScreen Labs", "Binary Horizon","Vertex Cloud", "PixelForge", "BrightCode", "Synapse Software", "Lunar Logic", "Stellar Systems", "EchoSoft",
+        "Zenith Digital", "Proton Software", "Atlas Technologies","CloudMatrix", "ByteWorks", "MetaNova", "Fusion Logic", "Aether Software", 
+        "Hexagon Studios", "Omega Interactive", "Astral Computing", "Nebula Technologies", "Quantum Engine","CodeStorm", "Skyline Software", 
+        "Pixel Dynamics", "Vertex Systems", "Nexus Digital", "Aurora Systems", "Prime Software", "Titan Systems", "DeepLogic", "MindMesh",};
+    static string[] Agronomia = {"Nexus CropScience", "Grupo Magenta", "Trinity Agriscience", " BASD", "Lara International",
+        "SQM - Sociedad Química y Minera", "Bunge", "Cargil", "Archer Daniels Midland - ADM", "Louis Dreyfus Company",
+        "CNH Industrial", "John Deere", "AGCO", "FMC Corporation", "UPL Limited","Sumitomo Chemical", "ADAMA", "Nufarm", "Limagrain", "KWS Saat",
+        "Rijk Zwaan", "DFL Seeds", "The Moisac Company", "OCP Gruop", "CF Industries", "EuroChem Group", "Trimble Agriculture", "Topcon Agriculture",
+        "Ceres Imaging", "Sakata Seed Corporation", "Takii & Co.", "East-West Seed", " Yuan Longping High-Tech Agriculture", "Enza Zaden", 
+        "Shandong Weifang Rainbow Chemical", "Sipcam Oxon Group", "Albaugh LLC", "PI Industries", "Certis Biologicals", "Helm AG", "ICL Group", 
+        "K+S Group", "Haifa Group", "Verdesian Life Sciense", "COFCO Group", "Wilmar International", "Olam International", "Netafim", 
+        "Valmont Industries", "Valley Irrigation", "Lindsay Corporation", "Zimmatic", "Spacegronomy"};
+    static string[] Comercio = {"Global Trade Corp", "Mercurio Logistics", "TradeSphere", "Comex International", "Blue Harbor Trading","Atlas Commerce",
+        "Golden Route Exports", "Nova Imports", "Pacific Exchange", "Prime Distribution","Urban Markets", "Capital Traders", "Titan Commerce", 
+        "Silver Bridge Trading", "Omni Retail Group","Vertex Commerce", "Metro Supply", "Infinity Traders", "Central Exchange Co.", "Summit Retail",
+        "InterWorld Logistics", "Mercantile Dynamics", "Horizon Trade Network", "Pioneer Imports", "Nexus Commerce","Bright Market Holdings", 
+        "Continental Trade Corp", "CrossOcean Exports", "United Distribution", "CommerceLink","Evergreen Retail", "Starlight Trading", "BluePeak Markets",
+        "Fusion Commerce", "SkyBridge Imports","Grand Bazaar Group", "TradePoint International", "Open Market Systems", "Velocity Distribution", "Urban Exchange",
+        "Golden Cartel", "Global Goods Network", "Prime Merchants", "Red Harbor Logistics", "Mercado Universal","Silver Road Commerce", "Frontier Traders", 
+        "Alpha Retail Group", "Metro Merchandising", "OceanGate Imports","Summit Trading Co.", "NorthStar Commerce", "BrightPath Logistics", "WorldConnect Trade", 
+        "Capital Market Group", "Liberty Commerce", "TradeCore International", "Unified Retail Systems", "Grand Exchange Holdings", "MercaNova" };
+    static string[] Alimenticio = { };
+    static string[] ManufacturaTextil = { };
+    static string[] Recursos = { };
+    static string[] ManufacturaDeRecursos = { };
+}
+
+
+
